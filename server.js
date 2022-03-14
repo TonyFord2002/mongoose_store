c587a4382778ca87a35036a112e368a1c8ab727d
TonyFord2002 <97993837+TonyFord2002@users.noreply.github.com>
2022-03-14 10:06:06 -0400

    all files created

---
+++ server.js
@@ -0,0 +1,31 @@
+require('dotenv').config()
+const express = require('express')
+const methodOverride= require('method-override')
+const app = express()
+const PORT = process.env.PORT || 3000
+
+//connect to database
+const db = require('./models/db')
+db.once('open', () => {
+  console.log('connected to mongo')
+})
+
+//Initialize View Engine
+app.set('view engine', 'jsx');
+app.engine('jsx', require('express-react-views').createEngine())
+
+// Mount Express Middleware
+app.use((req, res, next) => {
+  res.locals.data = {}
+  next()
+
+}) // Creates res.locals.data
+app.use(express.urlencoded({ extended: true })) // Creates req.body
+app.use(methodOverride('_method')); // Allows us to override methods
+app.use(express.static('public')); // Allows us to have Static Files
+
+
+// Listen on PORT
+app.listen(PORT, () => {
+  console.log('Making that money on:', PORT)
+})
